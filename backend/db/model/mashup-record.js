ea313b3e4b530504131b530d3d5dad9000bceecc
gin7018 <79063763+gin7018@users.noreply.github.com>
2024-03-31 17:00:45 -0400

    fix how data is parsed and loaded to db

--- backend/db/model/mashup-record.js
+++ backend/db/model/mashup-record.js
@@ -5,20 +5,25 @@ export function makeMashupRecord(db) {
         id: {type: String},
         title: {type: String},
         summary: {type: String},
-        rating: {type: String},
+        rating: {type: Number, default: 0.0},
         name: {type: String},
         label: {type: String},
         author: {type: String},
         description: {type: String},
         type: {type: String},
-        downloads : {type: String},
-        useCount : {type: String},
+        downloads : {type: Number, default: 0.0},
+        useCount : {type: Number, default: 0.0},
         sampleUrl: {type: String},
         dateModified: {type: String},
-        numComments : {type: String},
+        numComments : {type: Number, default: 0.0},
         commentsUrl: {type: String},
-        tags: {type: String},
-        apis: {type: String},
+        tags: [{type: String}],
+        apis: [{type: ApiSchema}],
         updated: {type: String}
     }));
 }
+
+const ApiSchema = new mongoose.Schema({
+    name: {type: String},
+    link: {type: String}
+})
